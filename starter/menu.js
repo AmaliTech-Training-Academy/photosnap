deb994e9af41974a9fe3d05c52d9fa0c3ff63218
PrinceCharlesTurkson <Elizabethben793@gmail.com>
2023-01-26 16:34:53 +0000

    working on the hamburger icon

---
+++ starter/menu.js
@@ -0,0 +1,37 @@
+const open = document.querySelector(".openMenu");
+open.addEventListener("click", fetchHam);
+ 
+function fetchHam() {
+    const menu = document.querySelector(".nav-links");
+    // const ham = document.querySelector(".openMenu");
+    menu.classList.add("active");
+    // menu.classList.remove("inactive");
+    
+}
+
+const close = document.querySelector(".openMenu");
+close.addEventListener("click", fetchClose);
+function fetchClose() {
+    const menu = document.querySelector(".nav-links");
+    const closeIcon = document.querySelector(".closeMenu");
+    closeIcon.classList.remove("inactive");
+    close.classList.add("inactive");
+    menu.classList.remove("active");
+    
+}
+
+
+// const open = document.querySelector(".openMenu");
+// const close = document.querySelector(".closeMenu"); 
+
+// const navMenu = document.querySelector(".navMenu").classList;
+
+// open.addEventListener("click",() => {
+//     navMenu.toggle("opened");
+//     navMenu.toggle("close");
+// });  
+
+// close.addEventListener("click",() => {
+//     navMenu.toggle("opened");
+//     navMenu.toggle("close");
+// });
